62917efa64d822e880b46040a4bc7d22a7e18627
Douglas Rocha <douglasbellonrocha@gmail.com>
2015-04-20 20:11:49 -0300

    Added Apache License 2.0

--- src/com/perfani/ddg/controller/FileController.java
+++ src/com/perfani/ddg/controller/FileController.java
@@ -1,3 +1,19 @@
+/***
+ *    Copyright 2015 Douglas Bellon Rocha
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.perfani.ddg.controller;
 
 import java.io.IOException;
@@ -17,7 +33,6 @@ import com.perfani.ddg.exceptions.EntityNotFoundException;
 import com.perfani.ddg.exceptions.InvalidKeyAmountException;
 import com.perfani.ddg.exceptions.InvalidMultiplicityException;
 import com.perfani.ddg.model.Entity;
-import com.perfani.ddg.model.Relationship;
 import com.perfani.ddg.utils.IOService;
 
 public class FileController
